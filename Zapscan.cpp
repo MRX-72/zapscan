169b73599faef902752f40efcdb276e9cd26e33f
MRX <94920917+MRX-72@users.noreply.github.com>
2022-05-16 16:19:24 +0530

    Update Zapscan.cpp

--- Zapscan.cpp
+++ Zapscan.cpp
@@ -1,25 +1,22 @@
 #include<iostream>
-#include<cstdlib>
+#include<ctime>
+#include<sstream>
+#include<chrono>
 #include<string>
 #include<unistd.h>
-#include<vector>
-#include<list>
-#include<sstream>
-#include<map>
 #include<utility>
 #include<time.h>
 
 #define xn std::cout<<"\n";
 #define l(x) std::cout<<x<<"\n";
-#define gln(x) std::getline(cin, x)
-
-using namespace std; //namespace                       
+#define gln(x) std::getline(cin, x)                                        
+using namespace std; //namespace
 unsigned int m_secn = 1000000; //global variable
 
 void dspl() {
         system("clear");
         xn xn
-        cout<<"                 #########################\n";
+        cout<<"         #########################\n";
         cout<<"         #                       #\n";
         cout<<"         #  Github:    MRX-72    #\n";
         cout<<"         #  Instagram: mr.x_cpp  #\n";
@@ -52,14 +49,19 @@ Commands:
 
     [-pz] -> Specific port scan
     [Example]:  zs -pz 127.0.0.1
+
+    [-Gz] -> Ping the Target
+    [O% packet loss  => Target is up]
         )";
     }
 
     else if(strcmp(argv[1],"zs")==0 && strcmp(argv[2],"-fz")==0)
     {
         dspl();
-        cout<<"-> S-TIME: "<<__DATE__; xn
-        cout<<"-> Exact : "<<__TIME__; xn xn
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
+        cout<<"-> S-Date: "<<__DATE__; xn
+        cout<<"-> T-Tkn : "<<__TIME__; xn xn
         cout<<"I> _Fast-Scan_"; xn
         cout<<" ______________________\n";
         cout<<"|PORT | STATE | SERVICE|\n";
@@ -70,13 +72,18 @@ Commands:
         xn xn
         cout<<"[zscan][fz]> Target ["<<argv[3]<<"] scanned";
         xn xn
-        cout<<"-> E-TIME: "<<__DATE__; xn
-        cout<<"-> Exact : "<<__TIME__;
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        cout<<"-> E-Date: "<<std::ctime(&end_time); xn
+        cout<<"-> T-Tkn : "<<elapsed_seconds.count()<<" seconds";
 
     }
     else if(strcmp(argv[1],"zs")==0 && strcmp(argv[2],"-bz")==0)
     {
         dspl();
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
         cout<<"-> S-TIME: "<<__DATE__; xn
         cout<<"-> Exact : "<<__TIME__; xn xn
         cout<<"_Basic-Scan_(Bit Slow)"; xn
@@ -89,13 +96,17 @@ Commands:
         xn xn
         cout<<"[zscan][bz]> Target ["<<argv[3]<<"] scanned";
         xn xn
-        cout<<"-> E-TIME: "<<__DATE__; xn
-        cout<<"-> Exact : "<<__TIME__;
-
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        cout<<"-> E-Date: "<<std::ctime(&end_time); xn
+        cout<<"-> T-Tkn : "<<elapsed_seconds.count()<<" seconds";
     }
     else if(strcmp(argv[1],"zs")==0 && strcmp(argv[2],"-vz")==0)
     {
         dspl();
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
         cout<<"-> S-TIME: "<<__DATE__; xn
         cout<<"-> Exact : "<<__TIME__; xn xn
         cout<<"_Vuln-PortScan_ (If - No ports shown => No vulnerable ports open"; xn
@@ -108,8 +119,11 @@ Commands:
         xn xn
         cout<<"[zscan][vz]> Target ["<<argv[3]<<"] scanned";
         xn xn
-        cout<<"-> E-TIME: "<<__DATE__; xn
-        cout<<"-> Exact : "<<__TIME__;
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        cout<<"-> E-Date: "<<std::ctime(&end_time); xn
+        cout<<"-> T-Tkn : "<<elapsed_seconds.count()<<" seconds";
 
     }
     else if(strcmp(argv[1],"zs")==0 && strcmp(argv[2],"-pz")==0)
@@ -122,6 +136,8 @@ Commands:
         cin>> y; xn
         cout<<"I> Scan Range ["<<x<<" - "<<y<<"]";
         xn xn
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
         cout<<"-> S-TIME: "<<__DATE__; xn
         cout<<"-> Exact : "<<__TIME__; xn xn
         cout<<" _____________________\n";
@@ -133,8 +149,34 @@ Commands:
         xn xn
         cout<<"[zscan][pz]> Target ["<<argv[3]<<"] scanned";
         xn xn
-        cout<<"-> E-TIME: "<<__DATE__; xn
-        cout<<"-> Exact : "<<__TIME__;
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        cout<<"-> E-Date: "<<std::ctime(&end_time); xn
+        cout<<"-> T-Tkn : "<<elapsed_seconds.count()<<" seconds";
+
+    }
+    else if(strcmp(argv[1],"zs")==0 && strcmp(argv[2],"-Gz")==0)
+    {
+        dspl();
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
+        cout<<"-> S-TIME: "<<__DATE__; xn
+        cout<<"-> Exact : "<<__TIME__; xn xn
+        cout<<"I> _Target-Ping_"; xn
+        xn
+        std::ostringstream oss;
+        cout<<"(I)> [Ping] ~ "; xn xn
+        oss<<"ping -c1 -s1 "<< argv[3]<<" | grep \"1 packet\" ";
+        system(oss.str().c_str());
+        xn
+        cout<<"[zscan][Gz]> Target ["<<argv[3]<<"] RSLT";
+        xn xn
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        cout<<"-> E-Date: "<<std::ctime(&end_time); xn
+        cout<<"-> T-Tkn : "<<elapsed_seconds.count()<<" seconds";
 
     }
     xn xn return 0;
