4248f2c7520591b4d3826b67d64906766f91e9c4
MRX <94920917+MRX-72@users.noreply.github.com>
2022-05-19 19:34:09 +0530

    Update req.sh

--- req.sh
+++ req.sh
@@ -1,2 +1,15 @@
-apt-get install nmap 
+!#/bin/bash
+
+echo '[Setup has started]\n\n'
+
 apt-get install clang
+apt-get install nmap
+
+g++ Zapscan.cpp -o zap
+
+mv zap $PATH
+
+echo 'Zap scan is successfully installed\n\n'
+
+echo 'Type: (zap zs --help) for more information\n\n'
+
